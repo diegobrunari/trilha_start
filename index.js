ab048a18d656e856dee34157822c22eff27af9a1
Diego Brunari <diego.skp@hotmail.com>
2023-09-26 20:55:54 -0300

    Adicionado botão para subir pagina

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-var elementosDuvida = document.querySelectorAll('.duvida')
+var elementosDuvida = document.querySelectorAll(".duvida");
 
 elementosDuvida.forEach(function (duvida) {
-    duvida.addEventListener('click', function(){
-        duvida.classList.toggle('ativa')
-    })
-})
+  duvida.addEventListener("click", function () {
+    duvida.classList.toggle("ativa");
+  });
+});
+
+function botaoSubir() {
+  if (window.scrollY !== 0) {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+}
+
+window.addEventListener("scroll", function () {
+  const botaoSubirTopo = document.getElementById("botaoSubir");
+  if (window.scrollY > 200) {
+    botaoSubirTopo.style.display = "block";
+  } else {
+    botaoSubirTopo.style.display = "none";
+  }
+});
